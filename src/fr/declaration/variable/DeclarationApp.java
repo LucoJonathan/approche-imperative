08621e3be1062d90cf7d5be96459d559147ce09a
Jonathan Luco <jonathan.luco44@gmail.com>
2024-04-04 14:26:32 +0200

    Fin du TP3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,34 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	byte a = 10;
+	short b = -17282;
+	int c = 2846325;
+	long d = 10000L;
+	float e = 2.5F;
+	double f = 6.4D;
+	char g = 'c';
+	boolean h = true;
+	String i = "chaine";
+	
+	String randomString = "Voic le résultat d'un calcul :\n 1+5=6";
+	
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	System.out.println(i);
+	
+	System.out.println(randomString);
+			
+	}
+
+}
